d140c1bede0a7fb276c285b02fd052264fb6bf29
Ramanpreet05k <msmily2005@gmail.com>
2025-07-28 16:48:58 +0530

    commit

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const SeatLayout = () => {
+  const {id, date} = useParams()
+
+  const [selectedSeats, setSelectedSeats] = useState([])
+
   return (
-    <div>SeatLayout</div>
+    <div>
+
+    </div>
   )
 }
 
